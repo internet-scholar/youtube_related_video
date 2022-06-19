c4b029d5e0d994196a8ff3ce335725ed2946ad5d
alexgonca <alexandre.reportagem@gmail.com>
2022-06-19 11:28:28 -0400

    from apt to apt-get

--- init_script.sh
+++ init_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo timedatectl set-timezone UTC
-sudo apt update -y
-sudo apt install -y python3-pip
+sudo apt-get update -y
+sudo apt-get install -y python3-pip
 cd /home/ubuntu
 wget https://raw.githubusercontent.com/internet-scholar/youtube_related_video/master/requirements.txt
 wget https://raw.githubusercontent.com/internet-scholar/youtube_related_video/master/youtube_related_video.py
